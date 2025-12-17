459421a10cd6e1576a186730ed2ef258af8db00d
光稳 <16462425+lightstability@user.noreply.gitee.com>
2025-12-17 14:20:58 +0800

    Initial commit

---
+++ yiyang-common/src/main/java/com/yiyang/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package com.yiyang.common.exception.user;
+
+import com.yiyang.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
